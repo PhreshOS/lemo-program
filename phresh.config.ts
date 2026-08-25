9792158fc0071312ea45b14166e5ba40f2448a34
zohayrslileh <zohayrslileh@gmail.com>
2026-08-25 04:34:23 +0100

    Release Lemo 0.1.4

--- phresh.config.ts
+++ phresh.config.ts
@@ -4,7 +4,8 @@ export default defineConfig({
     identity: "lemo",
     name: "Lemo",
     description: "The official PhreshOS agent.",
-    version: "0.1.3",
+    version: "0.1.4",
+    icon: "icon.png",
     buildCommand: "vite-node scripts/build.ts",
     server: {
         location: "dist/server",
